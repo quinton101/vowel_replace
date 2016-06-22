766036337921bf842cf8a989f7d0e13566d08526
John <john@epicodus.com>
2016-06-22 11:30:53 -0700

    edit boilerplate

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,23 @@ var myName = "John Franti";
 
 
 $(document).ready(function() {
+  // page setup logic
   $("#this_exercise span").text(project);
   $("#this_field span").text(project);
   $("#this_output span").text(project);
   $("#footer span").text(myName);
+
+  // page business logic
+  $("#input").submit(function(event) {
+    event.preventDefault();
+    $(".my_result").show();
+
+    // alert("Submission Successful");
+  });
+
+  $("#reset").click(function() {
+    $("#input_text").val("");
+    $(".my_result").hide();
+  })
 
 })
