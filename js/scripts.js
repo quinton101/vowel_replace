025ac28b7256bb41682949b83bd621f2a26fc7b0
John <john@epicodus.com>
2016-06-22 11:21:44 -0700

    setup boilerplate

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// FILL THIS OUT
+// name of exercise
+// name of student
+var project = "Leap Year";
+var myName = "John Franti";
+
+
+$(document).ready(function() {
+  $("#this_exercise span").text(project);
+  $("#this_field span").text(project);
+  $("#this_output span").text(project);
+  $("#footer span").text(myName);
+
+})
