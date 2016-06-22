695ed6f804f02b6b9da22f087582bd8c087ae144
Quinton <quintonw@live.com>
2016-06-22 16:47:13 -0700

    add vowel replace functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 // FILL THIS OUT
 // name of exercise
 // name of student
-var project = "{example}";
-var myName = "{student}";
+var project = "Vowel Replace";
+var myName = "Quinton and John";
+// business logic
+var vowels = ["a","e","i","o","u"];
+
+var replaceVowels = function(input) {
+    var inputArray = input.split("");
+    for(var i = 0; i < inputArray.length; i++) {
+        var result = vowels.indexOf(inputArray[i]);
+        if (result !== -1) {
+            inputArray[i] = "-";
+        }
+    }
+    var finalResult = inputArray.join("");
+    return finalResult;
+}
 
 
 $(document).ready(function() {
@@ -16,6 +30,8 @@ $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
     var userInput = $("#input_text").val();
+    var answer = replaceVowels(userInput);
+    $("#function_output span").text(answer);
     $(".my_result").show();
 
     // alert(userInput);
